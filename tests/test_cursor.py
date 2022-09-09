f1cecbb34558272c04a8014a9eb91518730726a8
Peng Ren <passren9099@hotmail.com>
2022-09-09 20:29:58 +0800

    Add fetchone test case

--- tests/test_cursor.py
+++ tests/test_cursor.py
@@ -6,11 +6,7 @@ class TestCursor:
     def test_writeone(self, cursor):
         sql_one_row_1_0_ = """
         INSERT INTO "%s" VALUE {
-            'key_partition': ?,
-            'key_sort': ?,
-            'col_str': ?,
-            'col_num': ?,
-            'col_byte': ?
+            'key_partition': ?, 'key_sort': ?, 'col_str': ?, 'col_num': ?, 'col_byte': ?
         }
         """ % self.table_name
         params_1_0_ = [
@@ -20,11 +16,7 @@ class TestCursor:
 
         sql_one_row_1_1_ = """
             INSERT INTO "%s" VALUE {
-                'key_partition': ?,
-                'key_sort': ?,
-                'col_str': ?,
-                'col_num': ?,
-                'col_map': ?
+                'key_partition': ?, 'key_sort': ?, 'col_str': ?, 'col_num': ?, 'col_map': ?
             }
         """ % self.table_name
         params_1_1_ = [
@@ -34,13 +26,7 @@ class TestCursor:
 
         sql_one_row_1_2_ = """
             INSERT INTO "%s" VALUE {
-                'key_partition': ?,
-                'key_sort': ?,
-                'col_str': ?,
-                'col_ss': ?,
-                'col_ns': ?,
-                'col_bs': ?,
-                'col_list': ?
+                'key_partition': ?, 'key_sort': ?, 'col_str': ?, 'col_ss': ?, 'col_ns': ?, 'col_bs': ?, 'col_list': ?
             }
         """ % self.table_name
         params_1_2_ = [
@@ -51,6 +37,23 @@ class TestCursor:
             ["Hello", "World", 1, 2, 3]
         ]
         cursor.execute(sql_one_row_1_2_, params_1_2_)
+
+        sql_one_row_2_1_ = """
+            INSERT INTO "%s" VALUE {
+                'key_partition': ?, 'key_sort': ?, 'col_nested_list': ?, 'col_nested_map': ?
+            }
+        """ % self.table_name
+        params_2_1_ = [
+            "test_one_row_2", 1, "test case 3", 
+            ["Hello", "World", 1.0, b"1", {1, 2, 3}, {"1", "2", "3"}, {"name": "test case 3", "version": 1.0}],
+            {
+                "name": "test case 3", 
+                "version": 1.0, 
+                "list": ["Hello", "World", {1, 2, 3}, {"1", "2"}, 2], 
+                "map": {"str": "Best", "num": 1, "chinese": u"你好"},
+            }
+        ]
+        cursor.execute(sql_one_row_2_1_, params_2_1_)
 
     def test_writemany(self, cursor):
         sql_many_rows_ = """
@@ -74,3 +77,29 @@ class TestCursor:
         ]
         cursor.executemany(sql_many_rows_, params_)
         assert len(cursor.errors) == 2
+
+    def test_fetchone(self, cursor):
+        cursor.execute("""
+            SELECT col_str, col_num, col_byte FROM "%s"
+            WHERE key_partition = ?
+            AND key_sort = ?
+        """ % self.table_name, [
+            "test_one_row_1", 0
+        ])
+        assert len(cursor.description) == 3
+        expected_data_ = []
+        for desc in cursor.description:
+            assert desc[0] in ["col_str", "col_num", "col_byte"]
+            if desc[0] == "col_str":
+                assert desc[1] == "S" 
+                expected_data_.append("test case 0")
+            if desc[0] == "col_num":
+                assert desc[1] == "N"
+                expected_data_.append(0)
+            if desc[0] == "col_byte":
+                assert desc[1] == "B"
+                expected_data_.append(b"0")  
+        assert cursor.rownumber == 0
+        assert cursor.fetchone() == tuple(expected_data_)
+        assert cursor.rownumber == 1
+        assert cursor.fetchone() is None
